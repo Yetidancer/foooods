d386d284566438fb43c978e2a8e080d2abc2c018
Ezekiel Clark <clarkzeke@gmail.com>
2020-03-30 10:42:22 -0600

    add figaro

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,7 @@ class FoodsController < ApplicationController
 
     conn = Faraday.new(url: "https://api.nal.usda.gov")
 
-    response = conn.get("/fdc/v1/search?api_key=MLnzkOtHdX2p98vw3SDTiyTXAqSctp5I6S0pMnlk&generalSearchInput=sweet%20potatoes")
+    response = conn.get("/fdc/v1/search?api_key=#{ENV["FDC_API_KEY"]}&generalSearchInput=sweet%20potatoes")
 
     @foods = JSON.parse(response.body, symbolize_names: true)[:foods].first(10)
 
